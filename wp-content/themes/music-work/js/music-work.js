04681494a7289307632ed74c5673d61d8f89a413
Kevin Morand <kevin@music-work.com>
2018-10-12 16:38:07 +0200

    Correctif design (alignement Service page)

--- wp-content/themes/music-work/js/music-work.js
+++ wp-content/themes/music-work/js/music-work.js
@@ -11,13 +11,13 @@ jQuery(document).ready(function() {
             $('.home header').css("background", "#fff");
         }, 5000);
     }else{
-        $('#preloader').delay(500).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-        $('body').delay(500).css({'overflow':'visible'});
+        $('#preloader').fadeOut('slow'); // will fade out the white DIV that covers the website. 
+        $('body').css({'overflow':'visible'});
         setTimeout(function(){
             $('.home .country-selector').css("opacity", "1");
             $('.home nav, .home #logo').css("opacity", "1");
             $('.home header').css("background", "#fff");
-        }, 500);
+        }, 1);
     }
 
     $('.divider-1 .divider-wrap:first-child > div').css('height', '20px');
